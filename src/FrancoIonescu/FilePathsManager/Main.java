11da6053b51cf79d0fdbb8a273edb24652dc9ae7
Franco <francoionescu@outlook.com>
2024-03-17 12:08:17 +0200

    input validations implemented

--- src/FrancoIonescu/FilePathsManager/Main.java
+++ src/FrancoIonescu/FilePathsManager/Main.java
@@ -27,6 +27,7 @@ public class Main {
                 case 2: displayPaths(filePaths);
                     break;
                 case 3: recycleBin = deletePaths(filePaths,recycleBin);
+                    filePaths = updatePathsAfterDelete(filePaths, recycleBin);
                     break;
                 case 4: displayRecycleBin(recycleBin);
                     break;
@@ -37,6 +38,7 @@ public class Main {
                 case 7: filePaths = copyFromFile(file, filePathsFromFile, filePaths);
                     break;
                 case 8:
+                    System.out.println("Exiting the program. Thank you for using our application!");
                     break;
             }
         }
@@ -85,35 +87,93 @@ public class Main {
     static String[] deletePaths(String[] filePaths, String[] recycleBin)
     {
         Scanner scan = new Scanner(System.in);
+        int option = 0;
         displayPaths(filePaths);
+        System.out.println(filePathIndex + 1 + ". " + "Exit");
         System.out.print("Enter the number for the path you want to delete: ");
-        while(!scan.hasNextInt())
-        {
-            System.out.print("Please enter a valid number: ");
-            scan.next();
+        while (true) {
+            if (scan.hasNextInt()) {
+                option = scan.nextInt();
+                if(option == filePathIndex + 1)
+                {
+                    break;
+                }
+                if (option >= 1 && option <= filePathIndex) {
+                    break;
+                } else {
+                    System.out.print("Please enter a valid number within the range: ");
+                }
+            } else {
+                System.out.print("Please enter a valid number: ");
+                scan.next();
+            }
         }
-        recycleBinIndex++;
-        int option = scan.nextInt();
-        recycleBin[recycleBinIndex - 1] = filePaths[option - 1];
-        recycleBin = Arrays.copyOf(recycleBin, recycleBinIndex + 1);
+        if(option != filePathIndex + 1)
+        {
+            recycleBinIndex++;
+            filePathIndex--;
+            recycleBin[recycleBinIndex - 1] = filePaths[option - 1];
+            recycleBin = Arrays.copyOf(recycleBin, recycleBinIndex + 1);
+        }
+
         return recycleBin;
+    }
+
+    static boolean isInRecycleBin(String filePath, String[] recycleBin) {
+        for (String path : recycleBin) {
+            if (filePath.equals(path)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    static String[] updatePathsAfterDelete(String[] filePaths, String[] recycleBin) {
+        int newSizeOfFilePath = filePaths.length - 1;
+        String[] updatedFilePaths = new String[newSizeOfFilePath];
+        int index = 0;
+
+        for (int i = 0; i < updatedFilePaths.length; i++) {
+            // Check if the current file path is not in the recycle bin
+            if (!isInRecycleBin(filePaths[i], recycleBin)) {
+                updatedFilePaths[index] = filePaths[i];
+                index++;
+            }
+        }
+        return updatedFilePaths;
     }
 
     static String[] recoverFromRecycleBin(String[] filePaths, String[] recycleBin)
     {
         Scanner scan = new Scanner(System.in);
+        int option = 0;
         displayRecycleBin(recycleBin);
+        System.out.println(recycleBinIndex + 1 + ". " + "Exit");
         System.out.print("enter the number for the path you want to recover: ");
-        while(!scan.hasNextInt())
-        {
-            System.out.print("Please enter a valid number: ");
-            scan.next();
+        while (true) {
+            if (scan.hasNextInt()) {
+                option = scan.nextInt();
+                if(option == recycleBinIndex + 1)
+                {
+                    break;
+                }
+                if (option >= 1 && option <= recycleBinIndex) {
+                    break;
+                } else {
+                    System.out.print("Please enter a valid number within the range: ");
+                }
+            } else {
+                System.out.print("Please enter a valid number: ");
+                scan.next();
+            }
         }
-        recycleBinIndex--;
-        int option = scan.nextInt();
-        String[] buffer = Arrays.copyOf(filePaths,filePaths.length + 1);
-        buffer[filePaths.length] = recycleBin[option - 1];
-        return buffer;
+        if(option != recycleBinIndex + 1)
+        {
+            recycleBinIndex--;
+            filePathIndex++;
+            filePaths = Arrays.copyOf(filePaths,filePaths.length + 1);
+            filePaths[filePaths.length - 1] = recycleBin[option - 1];
+        }
+        return filePaths;
     }
 
     static String[] readFromFile(File file, String[] filePathsFromFile)
@@ -121,12 +181,15 @@ public class Main {
         try
         {
             Scanner scan = new Scanner(file);
+            String path;
             int numberOfFiles = 0;
             int index = 0;
             while(scan.hasNextLine())
             {
                 String line = scan.nextLine();
-                numberOfFiles++;
+                if (line.contains("/") || line.contains("\\")) {
+                    numberOfFiles++;
+                }
             }
             filePathsFromFile = new String[numberOfFiles];
             numberOfFiles = 0;
@@ -135,9 +198,13 @@ public class Main {
             System.out.println();
             while(scan.hasNextLine())
             {
-                filePathsFromFile[index] = scan.nextLine();
-                System.out.println(index + 1 + ". " + filePathsFromFile[index]);
-                index++;
+                path = scan.nextLine();
+                if(path.contains("/") || path.contains("\\"))
+                {
+                    filePathsFromFile[index] = path;
+                    System.out.println(index + 1 + ". " + filePathsFromFile[index]);
+                    index++;
+                }
             }
             System.out.println();
             index = 0;
@@ -165,7 +232,7 @@ public class Main {
     {
         Scanner scan = new Scanner(System.in);
         int option = 0;
-        while(option != 5)
+        while(option != 8)
         {
             System.out.println("1. Add");
             System.out.println("2. Display paths");
@@ -183,27 +250,8 @@ public class Main {
                 System.out.println("Number out of bounds!");
                 System.out.println();
             }
-            switch(option)
-            {
-                case 1:
-                    return 1;
-                case 2:
-                    return 2;
-                case 3:
-                    return 3;
-                case 4:
-                    return 4;
-                case 5:
-                    return 5;
-                case 6:
-                    return 6;
-                case 7:
-                    return 7;
-                case 8:
-                    return 8;
-            }
+            return option;
         }
         return 0;
     }
 }
-
